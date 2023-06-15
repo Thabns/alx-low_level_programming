8fa5c6aab043f5bc7f7f3f857f4427a48aea4b0f
Rasejane Mothibeli <taicym5@gmail.com>
2023-06-15 10:27:00 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - A programe that prints the size of various computer types
+ * Return 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a chair: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
